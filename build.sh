ebccac74a0fb1663b7b70d6d2e3598853bc01e1a
Paul Sauer <paulrsauer@gmail.com>
2023-01-25 10:53:52 -0500

    Fix name

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 WOW_VERSION="100005"
-ADDON_VERSION="10.0.5.1"
+ADDON_VERSION="10.0.5.2"
 
 node --loader ts-node/esm --experimental-specifier-resolution=node ingest.ts
 rm -rf build
@@ -12,8 +12,8 @@ echo "}" >> build/PVP_Tooltips_Shadowlands/PVPTooltipsSL.lua
 cat addon/PVPTooltipsSL.lua >> build/PVP_Tooltips_Shadowlands/PVPTooltipsSL.lua
 
 echo "## Interface: $WOW_VERSION" > build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
-echo "## Title: PVP Tooltips: Shadowlands" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
-echo "## Notes: PvP tooltip changes" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
+echo "## Title: PVP Tooltips by WoW Arena Logs" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
+echo "## Notes: PvP tooltip changes inferred from spell database dumps" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
 echo "## Author: Armsperson" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
 echo "## Version: $ADDON_VERSION" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
 echo "" >> build/PVP_Tooltips_Shadowlands/PVP_Tooltips_Shadowlands.toc
